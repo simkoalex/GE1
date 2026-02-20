9477196d8ea9ae939534f54b79beba8711150a9e
Alexander Šimko <simkoalex@gmail.com>
2026-02-20 12:09:34 +0100

    improvement: GE2L01 - initial commit of a cpp project

---
+++ GE2FirstProject/Source/GE2FirstProject/GE2FirstProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GE2FirstProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GE2FirstProject, "GE2FirstProject" );
